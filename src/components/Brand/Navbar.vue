410ec4adc7163a3d3cd95384f5d271f578ac49cc
SeaCow <jazztyy@gmail.com>
2020-09-07 11:46:03 +0800

    餐車主頁

---
+++ src/components/Brand/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="bg-maincolor-400">
+        <nav class="container mx-auto px-5 flex justify-between items-center">
+            <h1 class="py-1 text-thirdcolor-500 text-5xl text-center">
+                <router-link to="/">Fotricle</router-link>
+            </h1>
+            <ul>
+                <li><a class="text-xl text-thirdcolor-400" href="cartorder.html">訂單系統</a></li>
+                <li><a class="text-xl text-thirdcolor-400 mr-5" href="restaurant.html">廚房端</a></li>
+                <li><a class="text-xl text-thirdcolor-400" href="cart.html">後台系統</a></li>
+                <li><button>登出</button></li>
+            </ul>
+        </nav>
+    </header>
+</template>
